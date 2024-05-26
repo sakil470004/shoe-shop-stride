d6d9a1bd794cdb8a8c967eb93e218d505074a97c
Mynul Islam <mynul.sakil@gmail.com>
2024-05-26 15:08:44 +0600

    fix banner

--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -6,16 +6,26 @@ const SingleProduct = ({ shoe }) => {
 
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
-      <figure>
-        <img src={image_url} alt="Shoes" />
+    {/* replace image bg as color */}
+
+
+
+      <figure className="bg-blue-500">
+        <img className="" src={image_url} alt="Shoes" />
       </figure>
-      <div className="card-body">
-        <h2 className="card-title">{title}</h2>
-        <h3 className="text-xl font-semibold">{brand}</h3>
-        <h3 className="text-xl font-semibold">{price}</h3>
-        <p>{description}</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">
+      <div className="card-body py-6">
+        <div className="flex justify-between">
+          <h2 className="card-title ">{title}</h2>
+          <h3 className="text-error font-bold text-xl"> ${price}</h3>
+        </div>
+        <h6 className="text-md  badge badge-outline badge-accent px-3 font-semibold">{brand}</h6>
+
+        <p className="font-extralight text-gray-500">
+          {description.slice(0, 35)}
+        </p>
+        <div className="flex justify-between mt-2">
+          <button className="btn btn-error text-white">Add To Cart</button>
+          <button className="btn btn-info text-white">
             <Link to={`/products/${id}`}>See details</Link>
           </button>
         </div>
--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -5,14 +5,12 @@ const Banner = () => {
     >
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
-        <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-          <p className="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+        <div className="max-w-lg">
+          <h1 className="mb-5 text-5xl font-bold">Welcome to Our Shoe Shop</h1>
+          <p className="mb-5 text-gray-200 font-thin">
+            Discover the latest trends in footwear at our store. We offer a wide range of styles and sizes. Find your perfect pair today!
           </p>
-          <button className="btn btn-primary">Get Started</button>
+          <button className="btn btn-primary">Shop Now</button>
         </div>
       </div>
     </div>
